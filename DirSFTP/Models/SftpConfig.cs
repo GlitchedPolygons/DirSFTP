838af6815c1324c4366d194409ca9236b4847ed6
Glitched Polygons <glitchedpolygons@protonmail.com>
2023-06-23 14:50:57 +0200

    Prepared code for when .NET MAUI will offer a way to display a text-input prompt that allows for the typed in characters to be hidden. Until then, the save password option will be forced on (they're saved in the SecureStorage anyway, so this should be fine for now). https://github.com/dotnet/maui/issues/15817

--- DirSFTP/Models/SftpConfig.cs
+++ DirSFTP/Models/SftpConfig.cs
@@ -25,7 +25,7 @@ public record SftpConfig
     public string Host { get; init; } = string.Empty;
     public int Port { get; init; } = 22;
     public string Username { get; init; } = string.Empty;
-    public string Password { get; init; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
     public string PrivateKey { get; init; } = null;
     public string PrivateKeyPassphrase { get; init; } = null;
     public string DefaultRemoteDirectory { get; init; } = ".";
